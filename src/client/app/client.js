b6b39c6483621f249f44b328e63275fbed3bb4da
nishantdania <nishantdania@gmail.com>
2016-10-20 19:34:35 +0530

    added boilerplate react code

---
+++ src/client/app/client.js
@@ -0,0 +1,12 @@
+console.log('hello world');
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+	render () {
+    return <p> Hello React!</p>;
+  
+	}
+}
+
+render(<App/>, document.getElementById('app'));
