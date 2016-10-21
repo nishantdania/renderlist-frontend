6b3633d35c0d5c230a0ba1c251719aa4fd6b96ed
nishantdania <nishantdania@gmail.com>
2016-10-21 15:57:16 +0530

    added node server

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ var webpack = require('webpack');
 var path = require('path');
 var CompressionPlugin = require("compression-webpack-plugin");
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var BUILD_DIR = path.resolve(__dirname, './build');
+var APP_DIR = path.resolve(__dirname);
 
 var config = {
 	entry: APP_DIR + '/client.js',
