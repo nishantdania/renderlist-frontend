1a7dde4792adc28d4d3af4225b34cd9b729a2266
nishantdania <nishantdania@gmail.com>
2016-10-21 19:42:08 +0530

    added react-router, root, about, notfound pages

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
