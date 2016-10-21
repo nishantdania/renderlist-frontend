7c4d85036158e65ba25b9234ce77af12c7ab893a
nishantdania <nishantdania@gmail.com>
2016-10-21 19:42:08 +0530

    added react-router, root, about, notfound pages

--- client.js
+++ client.js
@@ -1,12 +1,8 @@
-console.log('hello world');
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
-class App extends React.Component {
-	render () {
-    return <p> Hello React!</p>;
-  
-	}
-}
-
-render(<App/>, document.getElementById('container'));
+render ((
+	<Router history={ browserHistory }>{routes}</Router>
+), document.getElementById('container'))
