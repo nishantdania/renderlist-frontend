dd37f6e5c956d1c583910517aff3d6dc311edca6
nishantdania <nishantdania@gmail.com>
2016-10-24 13:41:33 +0530

    added base css

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
