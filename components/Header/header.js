3640ecfbcbc784854a112fd2c8ff4c9e84d412b7
nishantdania <nishantdania@gmail.com>
2016-10-26 19:35:42 +0530

    added nav links

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import cx from 'classnames';
 import styles from './header.css';
+import { Link  } from 'react-router';
 
 class Header extends Component {
 
@@ -8,6 +9,12 @@ class Header extends Component {
 		return <div className={cx(styles['container'])}>
 			<div className={cx(styles['innerBox'])}>
 				<div className={cx(styles['logo'])}>RenderList</div>
+				<div className={cx(styles['navbar'])}>
+					<ul className={cx(styles['navList'])}>
+						<li><Link to='/about'>About</Link></li>
+						<li>Login</li>
+					</ul>
+				</div>
 			</div>
 			<div className={cx(styles['subtitle'])}>
 				Find the best animation and film studios around the world ! 	
