383415f73666317aeb01805d754014943f436aee
nishantdania <nishantdania@gmail.com>
2016-10-26 13:37:50 +0530

    added token to local storage

--- routes.js
+++ routes.js
@@ -3,11 +3,13 @@ import { IndexRoute, Route } from 'react-router';
 import Root from './components/Root/root';
 import About from './components/About/about';
 import NotFound from './components/NotFound/notFound';
+import Redirect from './components/Redirect/redirect';
 
 const routes = ( 
 <Route>
 	<Route path='/' name='root' component={Root}/>
 	<Route path='/about' name='about' component={About}/>
+	<Route path='/redirect' name='redirect' component={Redirect}/>
 	<Route path='*' name='notFound' component={NotFound}/>
 </Route>
 );
