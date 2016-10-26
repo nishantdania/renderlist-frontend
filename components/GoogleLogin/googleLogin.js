fc78b844c23975c02d091a830fc0d2292c54f45d
nishantdania <nishantdania@gmail.com>
2016-10-26 21:51:14 +0530

    added google and fb login buttons

--- components/GoogleLogin/googleLogin.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default React.createClass({
-	render() {
-		return <div><a href='http://localhost:3000/api/auth/google?redirect=http://localhost:8080/redirect'>Login With Google</a></div>
-	}
-});
