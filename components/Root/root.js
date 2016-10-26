e2167f84869ddbe15955871dab56ee334bcb2a4e
nishantdania <nishantdania@gmail.com>
2016-10-26 12:56:55 +0530

    added google login component

--- components/Root/root.js
+++ components/Root/root.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import GoogleLogin from '../GoogleLogin/googleLogin';
 
 export default React.createClass({
 	render() {
-		return <div>Root</div>
+		return <div>
+			Root
+			<GoogleLogin/>
+		</div>
 	}
 });
