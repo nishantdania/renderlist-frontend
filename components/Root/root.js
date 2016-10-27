b5f1a6f9eb57e1a6e4a7066a384b458229c70909
nishantdania <nishantdania@gmail.com>
2016-10-27 13:54:02 +0530

    added footer component

--- components/Root/root.js
+++ components/Root/root.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Header/header';
+import Footer from '../Footer/footer';
 
 export default React.createClass({
 	render() {
 		return <div>
 			<Header/>
+			<Footer/>
 		</div>
 	}
 });
