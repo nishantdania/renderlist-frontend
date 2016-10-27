5f5ad646424ba798ec424b6d479505113d0ebce8
nishantdania <nishantdania@gmail.com>
2016-10-27 13:54:02 +0530

    added footer component

---
+++ components/Footer/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './footer.css';
+
+function Footer (props) {
+	return <div className={styles['outer']}>
+			© 2016 RenderList
+	</div>
+}
+
+export default Footer;
