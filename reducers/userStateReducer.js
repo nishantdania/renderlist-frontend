99cc8d63a3fbe0a1446494bf9b4a15e43b7b1f3e
nishantdania <nishantdania@gmail.com>
2016-10-29 12:24:40 +0530

    added userStateReducer and actions

--- reducers/userStateReducer.js
+++ reducers/userStateReducer.js
@@ -1,5 +1,25 @@
+import { GET_USER_STATE_SUCCESS, GET_USER_STATE_ERROR, USER_NOT_LOGGED_IN } from '../actions/userStateActions';
+
 export default function userState (state = {}, action = null) {
 	switch (action.type) {
+		case GET_USER_STATE_SUCCESS :
+			if (action.data.success) {
+				return Object.assign({}, state, { 
+					isLoggedIn : true,
+					user : action.data 
+				});
+			} else {
+				return Object.assign({}, state, {
+					isLoggedIn : false,
+					user : {}		
+				});
+			}
+		case GET_USER_STATE_ERROR :
+		case USER_NOT_LOGGED_IN :
+			return Object.assign({}, state, {
+				isLoggedIn : false,
+				user : {}	
+			});			
 		default:
 			return state;
 	}
