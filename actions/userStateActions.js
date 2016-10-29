f5405b8d49934aa6c61afddce2e93379bcf443f5
nishantdania <nishantdania@gmail.com>
2016-10-29 12:24:40 +0530

    added userStateReducer and actions

--- actions/userStateActions.js
+++ actions/userStateActions.js
@@ -4,6 +4,11 @@ import baseConfig, { headers } from './BaseConfig.js';
 export const GET_USER_STATE_REQUEST = 'GET_USER_STATE_REQUEST';
 export const GET_USER_STATE_SUCCESS = 'GET_USER_STATE_SUCCESS';
 export const GET_USER_STATE_ERROR = 'GET_USER_STATE_ERROR';
+export const USER_NOT_LOGGED_IN = 'USER_NOT_LOGGED_IN';
+
+export function userNotLoggedInAction () {
+	return {type: USER_NOT_LOGGED_IN};
+}
 
 export function getUserStateAction () {
 	return {type: GET_USER_STATE_REQUEST};
@@ -18,7 +23,10 @@ export function getUserStateErrorAction (error) {
 }
 
 export function fetchUserStateAction (requestHeaders) {
-	let token = localStorage.token || '';
+	let token = localStorage.token;
+	if (token === undefined) {
+		return dispatch(userNotLoggedInAction);
+	}
 	let reqBody = { 'token' : token };
 	return dispatch => {
 		dispatch(getUserStateAction());
