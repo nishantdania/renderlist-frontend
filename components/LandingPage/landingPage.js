113f5ffb2268b026c7d049c10478f8d5c7a5949c
nishantdania <nishantdania@gmail.com>
2016-10-30 17:39:57 +0530

    added landing page layout

---
+++ components/LandingPage/landingPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import styles from './landingPage.css';
+import cx from 'classnames';
+import HeaderMinimal from '../HeaderMinimal/headerMinimal';
+
+class LandingPage extends Component {
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+		return <div className={cx(styles['outer'])}>
+			<HeaderMinimal/>
+			<div className={cx(styles['title'])}>
+				Listing the showreels of artists and studios from around the world
+			</div>
+			<div className={cx(styles['subtitle'])}>
+				Showcase your work and be a part of the community. Join now to get listed for the launch.
+			</div>
+			<div className={cx(styles['cta'])}>
+				Join Now
+			</div>
+		</div>
+	}
+}
+
+export default LandingPage;
