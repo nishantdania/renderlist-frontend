454d0e3e163f64fb044993321e5ce6298ee61422
nishantdania <nishantdania@gmail.com>
2016-11-01 16:20:55 +0530

    add hasStudio param

--- routes.js
+++ routes.js
@@ -5,12 +5,14 @@ import About from './components/About/about';
 import NotFound from './components/NotFound/notFound';
 import Redirect from './components/Redirect/redirect';
 import LandingPage from './components/LandingPage/landingPage';
+import AddStudio from './components/AddStudio/addStudio';
 
 const routes = ( 
 <Route>
 	<Route name='root' component={Root}>
 		<Route path='/' component={LandingPage}/>
 		<Route path='/about' name='about' component={About}/>
+		<Route path='/addStudio' name='addStudio' component={AddStudio}/>
 		<Route path='/redirect' name='redirect' component={Redirect}/>
 		<Route path='*' name='notFound' component={NotFound}/>
 	</Route>
