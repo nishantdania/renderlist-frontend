9ea1cdf659ceb14a3d1a57599527a91dd1c8cc7f
nishantdania <nishantdania@gmail.com>
2016-11-02 16:04:57 +0530

    added style to studio form

--- components/AddStudio/addStudio.js
+++ components/AddStudio/addStudio.js
@@ -3,20 +3,19 @@ import styles from './addStudio.css';
 import cx from 'classnames';
 import Input from '../Input/input';
 import ButtonRound from '../ButtonRound/buttonRound';
+import TextArea from '../TextArea/textArea';
 
 class AddStudio extends Component {
 
 	render () {
-		return <div>
-			<div>
-				Add Your Studio
-				<Input title='Studio Name' placeholder='Enter your studio name' />
-				<Input title='Website URL' placeholder='http://www.johndoestudios.com'/>
-				<Input title='Description' placeholder='Enter your studio name'/>
-				<Input title='Showreel URL' placeholder='Enter your studio name'/>
-				<Input title='Location' placeholder='Enter your studio name'/>
-				<ButtonRound title='Submit'/>
-			</div>
+		return <div className={cx(styles['outer'])}>
+			<div className={cx(styles['title'])}>Add Your Studio</div>
+			<Input title='Studio Name' placeholder='Enter your studio name' />
+			<Input title='Website URL' placeholder='http://www.johndoestudios.com'/>
+			<TextArea title='Description' placeholder='Write something about your studio'/>
+			<Input title='Showreel URL' placeholder='Enter a Youtube or Vimeo link'/>
+			<Input title='City' placeholder='Enter your city'/>
+			<ButtonRound title='Submit' className={cx(styles['button'])}/>
 		</div>
 	}
 
