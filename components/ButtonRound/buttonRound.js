48c843fdd5e6e5fff2dec7b96d6fcb9a1142e721
nishantdania <nishantdania@gmail.com>
2016-11-02 23:10:25 +0530

    added onchange to cities input

--- components/ButtonRound/buttonRound.js
+++ components/ButtonRound/buttonRound.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 import styles from './buttonRound.css';
 import cx from 'classnames';
 
-function ButtonRound (props) {
-	return <div className={cx(styles['outer'], props.className)}>
-		<div>
-			{props.title}
+class ButtonRound extends Component {
+
+	constructor (props) {
+		super(props);
+	}
+
+	onClick () {
+		this.props.onClick();
+	}
+	
+	render () {
+		return <div onClick={this.onClick.bind(this)} className={cx(styles['outer'], this.props.className)}>
+			<div>
+				{this.props.title}
+			</div>
 		</div>
-	</div>
+	}
 }
 
 export default ButtonRound;
