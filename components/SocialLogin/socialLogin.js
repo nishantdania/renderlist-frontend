58eed798418fbdb183a10c58b8e7ac7815529c22
nishantdania <nishantdania@gmail.com>
2016-11-06 15:37:13 +0530

    added env variables

--- components/SocialLogin/socialLogin.js
+++ components/SocialLogin/socialLogin.js
@@ -2,20 +2,28 @@ import React from 'react'
 
 export default React.createClass({
 	render() {
-	let style = {
-		"maxWidth" : "200px",
-		"width" : "100%",
-		"marginTop" : "8px"
-	}
-		return <div>
-			<a href='http://localhost:3000/api/auth/google?redirect=http://localhost:8080/redirect'>
-				<img src='../assets/google_signin.png' style={style}></img>
-			</a>
-			<a href='http://localhost:3000/api/auth/facebook?redirect=http://localhost:8080/redirect'>
-				<img src='../assets/fb_signin.png' style={style}></img>
-			</a>
-
+		let style = {
+			"maxWidth" : "200px",
+			"width" : "100%",
+			"marginTop" : "8px"
+		}
+		return <div>{process.env.NODE_ENV == 'production'? <div>
+				<a href='http://www.renderlist.com/api/auth/google?redirect=http://www.renderlist.com/redirect'>
+					<img src='../assets/google_signin.png' style={style}></img>
+				</a>
+				<a href='http://www.renderlist.com/api/auth/facebook?redirect=http://www.renderlist.com/redirect'>
+					<img src='../assets/fb_signin.png' style={style}></img>
+				</a>
+			</div>
+			: <div>
+				<a href='http://localhost:3000/api/auth/google?redirect=http://localhost:8080/redirect'>
+					<img src='../assets/google_signin.png' style={style}></img>
+				</a>
+				<a href='http://localhost:3000/api/auth/facebook?redirect=http://localhost:8080/redirect'>
+					<img src='../assets/fb_signin.png' style={style}></img>
+				</a>
+			</div>
+		}
 		</div>
-
 	}
 });
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ var config = {
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env': {
-				  'NODE_ENV': JSON.stringify('production')
+				  'NODE_ENV': JSON.stringify(process.env.NODE_ENV) || '"development"'
 			}
 		}),	
 		new Extract('bundle.css',
