67d732c800ae20dc2993cc21cf6652524141db06
nishantdania <nishantdania@gmail.com>
2016-11-06 16:06:18 +0530

    added 404 page style

--- components/NotFound/notFound.js
+++ components/NotFound/notFound.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import styles from './notFound.css';
+import cx from 'classnames';
 
 export default React.createClass({
 	render() {
-		return <div><h1>Page not found</h1></div>
+		return <div className={cx(styles['outer'])}>Sorry ! The page you are looking for does not exist.</div>
 	}
 });
