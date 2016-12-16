ee522eb8b1a8e05dee58ddb9b534234dfc150e0e
nishantdania <nishantdania@gmail.com>
2016-12-17 01:14:23 +0530

    added basic featured grid

---
+++ components/FeaturedShowreelsGrid/featuredShowreelsGrid.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import cx from 'classnames';
+import styles from './featuredShowreelsGrid.css';
+
+class FeaturedShowreelsGrid extends Component {
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+		return <div className={cx(styles['main'])}>
+			<div className={cx(styles['inner'])}>
+				<div className={cx(styles['title'])}>Featured Showreel</div>
+				<div className={cx(styles['data'])}>
+					<img src='../assets/photo.jpg'/>
+					<span>Nishant Dania</span>
+				</div>
+			</div>
+		</div>
+	}
+}
+
+export default FeaturedShowreelsGrid;
