a0a3be976fee05c75633698af96826c5cd9932a3
nishantdania <nishantdania@gmail.com>
2016-12-21 21:29:44 +0530

    added video window

--- components/ShowreelGridUnit/showreelGridUnit.js
+++ components/ShowreelGridUnit/showreelGridUnit.js
@@ -2,22 +2,39 @@ import React, { Component } from 'react'
 import cx from 'classnames';
 import styles from './showreelGridUnit.css';
 import { Link } from 'react-router';
+import VideoWindow from '../VideoWindow/videoWindow';
 
 class ShowreelGridUnit extends Component {
 	constructor (props) {
 		super(props);
+		this.state = {
+			openVideo : false
+		}
+	}
+
+	handlePlayClicked () {
+		this.setState({
+			openVideo : true
+		});
+	}
+
+	closeVideo () {
+		this.setState({
+			openVideo : false
+		});
 	}
 
 	render () {
 		const showreel = this.props.showreel;
 		const link = '/' + this.props.showreel.username;
 		return <div>{showreel ? <div className={cx(styles['main'])}>
+			{this.state.openVideo ? <VideoWindow closeVideo={this.closeVideo.bind(this)} url='https://player.vimeo.com/video/59777392?title=0&byline=0'/> : null}
 			<div className={cx(styles['container'])}>
 				<div className={cx(styles['upper-container'])}>
 					<div className={cx(styles['video-container'])}>
 						<img src={showreel.thumbnail}/>
 					</div>
-					<div className={cx(styles['play-button-container'])}>
+					<div onClick={this.handlePlayClicked.bind(this)} className={cx(styles['play-button-container'])}>
 						<div className={cx(styles['play-button'])}>
 							<img src='../assets/play-icon.svg'/>
 						</div>
