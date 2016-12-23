b2cabe352f01863db0ce86ca958c6b78c2814c92
nishantdania <nishantdania@gmail.com>
2016-12-23 21:02:16 +0530

    added basic search call

--- actions/showreelListActions.js
+++ actions/showreelListActions.js
@@ -4,6 +4,8 @@ import baseConfig, { headers } from './BaseConfig.js';
 export const VERIFIED_SHOWREELS_REQUEST = 'VERIFIED_SHOWREELS_REQUEST';
 export const VERIFIED_SHOWREELS_SUCCESS = 'VERIFIED_SHOWREELS_SUCCESS';
 export const VERIFIED_SHOWREELS_ERROR = 'VERIFIED_SHOWREELS_ERROR';
+export const SEARCH_SHOWREELS_SUCCESS = 'SEARCH_SHOWREELS_SUCCESS';
+export const SEARCH_SHOWREELS_ERROR = 'SEARCH_SHOWREELS_ERROR';
 
 export function getVerifiedShowreelsRequestAction () {
 	return {type : VERIFIED_SHOWREELS_REQUEST};
@@ -17,6 +19,14 @@ export function getVerifiedShowreelsErrorAction (error) {
 	return {type : VERIFIED_SHOWREELS_ERROR, error};
 }
 
+export function searchShowreelsSuccessAction (data) {
+	return {type : SEARCH_SHOWREELS_SUCCESS, data};
+}
+
+export function searchShowreelsErrorAction (error) {
+	return {type : SEARCH_SHOWREELS_ERROR, error};
+}
+
 export function getVerifiedShowreelsAction () {
 	return dispatch => {
 		dispatch(getVerifiedShowreelsRequestAction());
@@ -25,6 +35,19 @@ export function getVerifiedShowreelsAction () {
 			headers: Object.assign({}, headers)
 			})).then(json => dispatch(getVerifiedShowreelsSuccessAction(json)), err => {
 				dispatch(getVerifiedShowreelsErrorAction(err));
+			return Promise.reject(err);
+			});
+	};
+}
+
+export function searchShowreelsAction (query) {
+	let reqBody = {"searchQuery" : query};
+	return dispatch => {
+		return ApiCaller.post(Object.assign({}, baseConfig, {
+			pathname : '/api/search',
+			headers: Object.assign({}, headers)
+			}), reqBody).then(json => dispatch(searchShowreelsSuccessAction(json)), err => {
+				dispatch(searchShowreelsErrorAction(err));
 			return Promise.reject(err);
 			});
 	};
