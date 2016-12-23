d4ae2d98fbe28d476158957565dc164c7270e357
nishantdania <nishantdania@gmail.com>
2016-12-23 21:02:16 +0530

    added basic search call

--- reducers/showreelListReducer.js
+++ reducers/showreelListReducer.js
@@ -1,4 +1,4 @@
-import { VERIFIED_SHOWREELS_REQUEST, VERIFIED_SHOWREELS_SUCCESS, VERIFIED_SHOWREELS_ERROR } from '../actions/showreelListActions';
+import { VERIFIED_SHOWREELS_REQUEST, VERIFIED_SHOWREELS_SUCCESS, VERIFIED_SHOWREELS_ERROR , SEARCH_SHOWREELS_SUCCESS, SEARCH_SHOWREELS_ERROR} from '../actions/showreelListActions';
 import {INIT, LOADING, SUCCESS, ERROR} from '../utils/asyncStatusHelper';
 
 export default function showreelList (state = {
@@ -10,7 +10,13 @@ export default function showreelList (state = {
 				asyncStatus : SUCCESS,
 				data : action.data.showreels
 			});
+		case SEARCH_SHOWREELS_SUCCESS :
+			return Object.assign({}, state, { 
+				asyncStatus : SUCCESS,
+				searchData : action.data
+			});
 		case VERIFIED_SHOWREELS_ERROR :
+		case SEARCH_SHOWREELS_ERROR :
 			return Object.assign({}, state, {
 				asyncStatus : ERROR	
 			});			
