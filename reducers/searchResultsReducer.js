8d494140e32516bb596ceabf0d41aad0376f5103
nishantdania <nishantdania@gmail.com>
2016-12-23 22:27:27 +0530

    added search results reducer

--- reducers/searchResultsReducer.js
+++ reducers/searchResultsReducer.js
@@ -1,16 +1,16 @@
-import { VERIFIED_SHOWREELS_REQUEST, VERIFIED_SHOWREELS_SUCCESS, VERIFIED_SHOWREELS_ERROR } from '../actions/showreelListActions';
+import {  SEARCH_SHOWREELS_SUCCESS, SEARCH_SHOWREELS_ERROR } from '../actions/showreelListActions';
 import {INIT, LOADING, SUCCESS, ERROR} from '../utils/asyncStatusHelper';
 
-export default function showreelList (state = {
+export default function searchResults (state = {
 			asyncStatus : INIT
 		}, action = null) {
 	switch (action.type) {
-		case VERIFIED_SHOWREELS_SUCCESS :
+		case SEARCH_SHOWREELS_SUCCESS :
 			return Object.assign({}, state, { 
 				asyncStatus : SUCCESS,
 				data : action.data.showreels
 			});
-		case VERIFIED_SHOWREELS_ERROR :
+		case SEARCH_SHOWREELS_ERROR :
 			return Object.assign({}, state, {
 				asyncStatus : ERROR	
 			});			
