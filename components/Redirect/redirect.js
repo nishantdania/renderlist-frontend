6c31e83b79c6cb1144a1ded98840ed86e0518594
nishantdania <nishantdania@gmail.com>
2017-01-09 12:32:06 +0530

    bug fixes

--- components/Redirect/redirect.js
+++ components/Redirect/redirect.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 var Router = require('react-router');
+import { connect  } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import { fetchUserStateAction } from '../../actions/userStateActions.js';
 
 class Redirect extends Component {
 	
 	componentDidMount () {
 		if(window.localStorage) {
 			localStorage.setItem("token", this.props.location.query.token);
+			if (!this.props.userState.isLoggedIn) this.props.fetchUserState();
 		}
 		if(this.props.location.query.hasStudio) {
 			Router.browserHistory.replace('/');
@@ -22,5 +26,17 @@ class Redirect extends Component {
 	};
 };
 
-export default Redirect;
+function mapStateToProps (state) {
+	return {
+		userState : state.userState
+	};
+}
+
+function mapDispatchToProps (dispatch) {
+	return bindActionCreators({
+		fetchUserState : fetchUserStateAction
+	}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Redirect);
 
